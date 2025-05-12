f700d2edc68e599c183b3068a16f5f79963724ca
Mark Tully <marktully2012@gmail.com>
2025-05-12 12:59:12 +0100

    Import local project initial work

---
+++ src/main/GamePanel.java
@@ -0,0 +1,104 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+    // Screen Settings
+    final int originalTileSize = 16; //this is 16x16 tile
+
+    //now I have to scale the 16x16 tile to fit today's monitor/screen size (16 is based on SEGA architecture)
+    final int scale = 3; //set the scale to be 3 times bigger... 3x16 = 48 (a common for game dev)
+    final int tileSize = originalTileSize * scale; //48x48 tile
+
+    //now we have to how many tiles can be displayed on a single screen
+    final int maxScreenCol = 16; //max titles on x-axis is 16
+    final int maxScreenRow = 12; //max titles on y-axis is 12
+
+    final int screenWidth = tileSize * maxScreenCol; // 16x48 = 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; //12x48 = 576 pixels
+
+    //we need to instantiate the key handler
+    KeyHandler keyH = new KeyHandler();
+
+    //we need time for the game to be run, allowing us to make a refresh rate etc.
+    //hence, we use a 'Thread'
+    Thread gameThread; //a thread that you can start and stop
+
+    //set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel(){
+        //set the size of the class 'JPanel'
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        //set background color to black
+        this.setBackground(Color.black);
+        //set the buffer to true, all the drawing from this component will be done in an offscreen painting buffer
+        // i.e. the rendering performance of the game is enhanced
+        this.setDoubleBuffered(true);
+
+        //add key handler to game panel
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    //the 'run' method is overwritten from the interface 'Runnable'
+    //when we start the game's thread, this run method is automatically called
+
+    public void startGameThread(){
+        gameThread = new Thread(this); //calling 'this' refers to the class 'GamePanel', i.e. we are passing the class into this constructor
+        gameThread.start();
+    }
+
+    //this run method will contain the core running of the game, it will hold the game loop
+    @Override
+    public void run(){
+        //as long as the gameThread exists, this process is repeated forever
+        while(gameThread != null){
+
+            // To check the current time while the game runs
+            long currentTime = System.nanoTime();
+
+            //1. We have to update the information, such as character positioning
+            update();
+            //2. Redraw the screen
+            //redrawing the screen 60 times per second, is 60 FPS
+            repaint(); //this is how you call the 'paintComponent' method
+        }
+    }
+
+    public void update(){
+        //update player position
+        if(keyH.upPressed){
+            playerY -= playerSpeed;
+        }
+        if(keyH.downPressed){
+            playerY += playerSpeed;
+        }
+        if(keyH.leftPressed){
+            playerX -= playerSpeed;
+        }
+        if(keyH.rightPressed){
+            playerX += playerSpeed;
+        }
+    }
+
+    //method used to redraw onto the screen
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        //now we call the class 'Graphics2D' class, extending the Graphics class
+        /*This provides a more sophisticated control over geometry, coordinate transformations, color management and text layout*/
+        Graphics2D g2 = (Graphics2D) g; //here we do type conversion
+
+        //draw the player
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, tileSize, tileSize); //starting at (100, 100) and is just a white shape
+
+        //the 'dispose' method is used to dispose of this graphics context and release any system resources that it is using
+        g2.dispose();
+
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+//belongs to the package 'main' in src
+package main;
+
+//import for JFrame
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        //Create a window first for the game
+        JFrame window = new JFrame();
+        //window setting configurations
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+
+        //Game panel is now to be included
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        //set the window to the centre of the screen
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
